914b910640e2ead524e320141fd1bb36553b2914
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-02-08 03:01:40 +0000

    Working infinite world generation

--- Assets/Scripts/GroundCreator.cs
+++ Assets/Scripts/GroundCreator.cs
@@ -8,10 +8,10 @@ using UnityEngine;
 public class GroundCreator : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int Width;
-    public int Height;
+    public int Width = 100;
+    public int Height = 100;
 
-    public float TriangleSize;
+    public float TriangleSize = 1;
 
     [Header("Coarse Noise")]
     public float CoarsePerlinWidthScale;
@@ -24,6 +24,13 @@ public class GroundCreator : MonoBehaviour
     private MeshRenderer rend;
     private MeshFilter filter;
 
+    [HideInInspector]
+    public int ChunkX = 0;
+    [HideInInspector]
+    public int ChunkY = 0;
+
+    private bool HasGenerated = false;
+
     public void Start()
     {
         rend = GetComponent<MeshRenderer>();
@@ -33,42 +40,44 @@ public class GroundCreator : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !HasGenerated)
         {
-            Generate();
+            GenerateSurrounding();
+            HasGenerated = true;
         }
     }
     public void Generate()
     {
-        rend.sharedMaterial = new Material(Shader.Find("Standard"));
+        //rend.sharedMaterial = new Material(Shader.Find("Standard"));
 
         Mesh mesh = new Mesh();
 
         List<Vector3> VerticesList = new List<Vector3>();
+        print("Chunk: " + ChunkX + " " + ChunkY + ": " + (ChunkX * Width) + ", " + (ChunkY * Height));
+        for (int y = 0; y < Height + 1; y++)
+        {
+            for (int x = 0; x < Width + 1; x++)
+            {
+                float height = Mathf.PerlinNoise((x + (ChunkX * Width)) * CoarsePerlinWidthScale, (y + (ChunkY * Width)) * CoarsePerlinWidthScale) * CoarsePerlinHeightScale +
+                               Mathf.PerlinNoise((x + (ChunkX * Width)) * FinePerlinWidthScale, (y + (ChunkY * Width)) * FinePerlinWidthScale) * FinePerlinHeightScale;
+                VerticesList.Add(new Vector3(x * TriangleSize, height, y * TriangleSize));
+            }
+        }
+        mesh.vertices = VerticesList.ToArray();
+
+        List<int> TrisList = new List<int>();
 
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width; x++)
             {
-                float height = Mathf.PerlinNoise(x * CoarsePerlinWidthScale, y * CoarsePerlinWidthScale) * CoarsePerlinHeightScale + Mathf.PerlinNoise(x * FinePerlinWidthScale, y * FinePerlinWidthScale) * FinePerlinHeightScale;
-                VerticesList.Add(new Vector3(x * TriangleSize, height, y * TriangleSize));
-            }
-        }
-        mesh.vertices = VerticesList.ToArray(); ;
+                TrisList.Add(y * (Width + 1) + x);
+                TrisList.Add((y + 1) * (Width + 1) + x);
+                TrisList.Add(y * (Width + 1) + x + 1);
 
-        List<int> TrisList = new List<int>();
-
-        for (int y = 0; y < Height - 1; y++)
-        {
-            for (int x = 0; x < Width - 1; x++)
-            {
-                TrisList.Add(y * Width + x);
-                TrisList.Add((y + 1) * Width + x);
-                TrisList.Add(y * Width + x + 1);
-
-                TrisList.Add((y + 1) * Width + x);
-                TrisList.Add((y + 1) * Width + x + 1);
-                TrisList.Add(y * Width + x + 1);
+                TrisList.Add((y + 1) * (Width + 1) + x);
+                TrisList.Add((y + 1) * (Width + 1) + x + 1);
+                TrisList.Add(y * (Width + 1) + x + 1);
             }
         }
 
@@ -78,4 +87,12 @@ public class GroundCreator : MonoBehaviour
 
         filter.mesh = mesh;
     }
+
+    public void GenerateSurrounding()
+    {
+        Instantiate(this.gameObject, transform.position + new Vector3(Width * TriangleSize, 0, 0), Quaternion.identity).GetComponent<GroundCreator>().ChunkX = ChunkX + 1;
+        Instantiate(this.gameObject, transform.position + new Vector3(0, 0, Height * TriangleSize), Quaternion.identity).GetComponent<GroundCreator>().ChunkY = ChunkY + 1;
+    }
+
+
 }
