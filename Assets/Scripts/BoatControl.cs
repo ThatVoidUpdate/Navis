2e675bbae269c3f460eb38795f750bfd2546bcdf
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-02-29 13:59:24 +0000

    Started working on boat controls

---
+++ Assets/Scripts/BoatControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BoatControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float Speed;
+    public GameObject Propeller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForceAtPosition(Vector3.forward * Speed, transform.InverseTransformPoint(Propeller.transform.position));
+        }
+    }
+}
